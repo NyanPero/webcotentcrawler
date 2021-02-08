54daebe90eb637388dd9c04830bd358a52ba9889
NyanPero <hiroki.nakano.826@gmail.com>
2021-02-08 19:25:42 +0900

    modify save file name

--- go-spider.py
+++ go-spider.py
@@ -1,7 +1,6 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 import click
-import datetime
 import pandas as pd
 import itertools
 from io import StringIO
@@ -20,12 +19,11 @@ def get_crawl_url_list(input_file):
 def check_twitter_suspend(input_file, output_dir, user_agent, simulate_os):
 
     url_list = get_crawl_url_list(input_file)
+    file_name = input_file.split("/")[-1].replace('.jsonl','')
     settings = get_project_settings()
-    
-    now_str = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
     # file out path
-    settings.set('FEED_URI', output_dir+'{}_webcontent_{}.jsonl'.format(now_str,simulate_os))
+    settings.set('FEED_URI', output_dir+'{}_webcontent_{}.jsonl'.format(file_name, simulate_os))
     
     process = CrawlerProcess(settings)
 
